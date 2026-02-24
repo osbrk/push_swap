20c83b0bb67087ba74c4db7a7cf1670535d6e299
osbrk <135859764+osbrk@users.noreply.github.com>
2026-02-24 14:49:13 +0100

    Starting structure

---
+++ arg_check.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg_check.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osukhore <osukhore@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 14:35:04 by osukhore          #+#    #+#             */
+/*   Updated: 2026/02/24 14:48:04 by osukhore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	arg_check(const char *str)
+{
+
+}
+
+// What do we want to do here?
+// 1. FT_SPLIT using the ' ' [space]
+// 2. Check each resulting string if: numeric, accepts +/-, repeats
+// 3. Errors to be printed in separate function.c
---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osukhore <osukhore@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 14:39:40 by osukhore          #+#    #+#             */
+/*   Updated: 2026/02/24 14:43:34 by osukhore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+int		arg_check(const char *str)
+
+# endif //PUSH_SWAP_H
