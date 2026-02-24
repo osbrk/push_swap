1ea7c7f78e904279907c80c6add3d66259b6cbf2
osbrk <135859764+osbrk@users.noreply.github.com>
2026-02-24 15:32:07 +0100

    Small edit push

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: osukhore <osukhore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 15:11:07 by osukhore          #+#    #+#             */
-/*   Updated: 2026/02/24 15:29:03 by osukhore         ###   ########.fr       */
+/*   Updated: 2026/02/24 15:31:12 by osukhore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ int	main(int argc, char **argv)
 	i = 1;
 	if (argc < 2)
 		return(printf("Error\n"));
-
 	while (i < argc)
 	{
 		if (ft_num_check(argv[i]) == 1)
